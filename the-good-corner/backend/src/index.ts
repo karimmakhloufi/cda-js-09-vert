ce4565ae8147fc7a2c219a7daf5ec6693eac3f65
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-09-19 12:25:28 +0200

    Added get and post for ads

--- the-good-corner/backend/src/index.ts
+++ the-good-corner/backend/src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
 
+import { ads } from "./data";
+
 const app = express();
 const port: number = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World from typescript app with live reload");
+});
+
+app.get("/ad", (req, res) => {
+  res.send(ads);
+});
+
+app.post("/ad", (req, res) => {
+  console.log(req.body);
+  ads.push(req.body);
+  res.send("The ad has been added");
 });
 
 app.listen(port, () => {
