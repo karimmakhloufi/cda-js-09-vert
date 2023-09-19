cb0a1de052c7f197c4369c244ad70d30e1baa387
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-09-19 09:46:07 +0200

    Finished TS exercices

--- lc-typescript/src/index.ts
+++ lc-typescript/src/index.ts
@@ -10,14 +10,35 @@ type Message = { from: string; message: string; isRead?: boolean };
 const message1: Message = {
   from: "Bob",
   message: "Hello Alice!",
-  isRead: true,
+  isRead: false,
 };
 const message2: Message = { from: "Alice", message: "Hi Bob!" };
 
-const messages = [message1, message2]; // type this line
-function readMessages(messages) {
+const messages: Message[] = [message1, message2]; // type this line
+
+function readMessages(messages: Message[]) {
   // this one too
   // put some logic to set isRead to true on all messages
-  return 0;
+  messages.forEach((el) => {
+    el.isRead = true;
+  });
+
+  return messages.length;
 }
-readMessages(messages);
+console.log(readMessages(messages));
+type Notif = { at: number; state: string };
+
+const notification1: Notif = { at: 1694011133, state: "unread" };
+const notification2: Notif = { at: 1694011532, state: "read" };
+
+type NotifOrMessage = Notif | Message;
+
+function readNotificationOrMessage(
+  notificationOrMessage: NotifOrMessage
+): void {
+  if ("isRead" in notificationOrMessage) {
+    notificationOrMessage.isRead = true;
+  } else if ("state" in notificationOrMessage) {
+    notificationOrMessage.state = "read";
+  }
+}
