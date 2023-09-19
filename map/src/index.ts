dfea33fe5cd0a67a60304049d41d3d8316ad7434
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-09-19 16:35:46 +0200

    map and foreach

---
+++ map/src/index.ts
@@ -0,0 +1,28 @@
+let numbers = [1, 2, 3, 4];
+
+console.log(numbers);
+
+numbers = numbers.map((el) => el * 2);
+
+console.log(numbers);
+
+let students = [
+  { age: 25, name: "Alice" },
+  { age: 26, name: "Bob" },
+  { age: 27, name: "Cedric" },
+  { age: 28, name: "Damien" },
+  { age: 29, name: "Eleonore" },
+];
+
+// const index = students.findIndex((student) => student.name === "Damien");
+
+// students[index].age = 30;
+
+students.forEach((student) => {
+  if (student.name === "Damien") {
+    student.age = 30;
+  }
+  console.log(student);
+});
+
+// console.log(students);
