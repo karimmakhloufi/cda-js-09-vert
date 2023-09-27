d2efb9342486c1d804b81f5e3c0866ca4f5da460
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-09-27 14:23:06 +0200

    Frontend now sends data to backend in new add page

--- the-good-corner/frontend/src/pages/ad/new.tsx
+++ the-good-corner/frontend/src/pages/ad/new.tsx
@@ -1,4 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+type category = {
+  id: number;
+  name: string;
+};
+
 const NewAd = () => {
+  const [categories, setCategories] = useState<category[]>([]);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const result = await axios.get<category[]>(
+          "http://localhost:4000/category"
+        );
+        setCategories(result.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchCategories();
+  }, []);
   return (
     <form
       onSubmit={(e) => {
@@ -12,12 +34,46 @@ const NewAd = () => {
         // Or you can work with it as a plain object:
         const formJson = Object.fromEntries(formData.entries());
         console.log(formJson);
+        axios.post("http://localhost:4000/ad", formJson);
       }}
     >
       <label>
         Titre de l&apos;annonce: <br />
-        <input className="text-field" name="titre" />
+        <input className="text-field" name="title" />
       </label>
+      <br />
+      <label>
+        Prix: <br />
+        <input className="text-field" name="price" />
+      </label>
+      <br />
+      <label>
+        Description: <br />
+        <input className="text-field" name="description" />
+      </label>
+      <br />
+      <label>
+        Nom du vendeur: <br />
+        <input className="text-field" name="owner" />
+      </label>
+      <br />
+      <label>
+        Url de l&apos;image: <br />
+        <input className="text-field" name="imageUrl" />
+      </label>
+      <br />
+      <label>
+        Ville: <br />
+        <input className="text-field" name="location" />
+      </label>
+      <br />
+      <select name="category">
+        {categories.map((category) => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </select>
       <button className="button">Submit</button>
     </form>
   );
