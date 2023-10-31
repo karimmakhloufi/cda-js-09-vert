db1432534887e9e56fde1884331667b2819d2b84
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-10-31 11:49:41 +0100

    Typegraphql for backend

--- graphql/backend/src/index.ts
+++ graphql/backend/src/index.ts
@@ -1,63 +1,77 @@
+import "reflect-metadata";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+import {
+  Arg,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  buildSchema,
+} from "type-graphql";
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    getAllBooks: [Book]
-    getOneBook: Book
-  }
-  type Mutation {
-    deleteBookById: String
-  }
-`;
-
-const booksDB = [
+const booksDB: Book[] = [
   {
+    id: "0",
     title: "The Awakening",
     author: "Kate Chopin",
   },
   {
+    id: "1",
     title: "City of Glass",
     author: "Paul Auster",
   },
 ];
 
-const resolvers = {
-  Query: {
-    getAllBooks: () => booksDB,
-    getOneBook: () => booksDB[0],
-  },
-  Mutation: {
-    deleteBookById: () => "book was deleted",
-  },
-};
+@ObjectType()
+class Book {
+  @Field()
+  id: string;
+  @Field()
+  title: string;
+  @Field()
+  author: string;
+}
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+@InputType()
+class BookInput {
+  @Field()
+  title: string;
+
+  @Field()
+  author: string;
+}
+
+@Resolver()
+class BookResolver {
+  @Query(() => [Book])
+  getAllBooks() {
+    return booksDB;
+  }
+
+  @Query(() => Book)
+  getBookById(@Arg("id") id: string) {
+    return booksDB.find((book) => book.id == id);
+  }
+
+  @Mutation(() => Book)
+  addBook(@Arg("data") { title, author }: BookInput) {
+    const lastId = parseInt(booksDB.at(-1)?.id as string, 10);
+    const id = (lastId + 1).toString();
+    booksDB.push({ title, author, id });
+    return booksDB.at(-1);
+  }
+}
 
 const start = async () => {
-  // Passing an ApolloServer instance to the `startStandaloneServer` function:
-  //  1. creates an Express app
-  //  2. installs your ApolloServer instance as middleware
-  //  3. prepares your app to handle incoming requests
+  const schema = await buildSchema({
+    resolvers: [BookResolver],
+  });
+
+  const server = new ApolloServer({ schema });
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
   });
