853a145445794b159ba137ad08c7ea5a5fb4d7f1
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-10-31 09:48:05 +0100

    Backend graphql 101

---
+++ graphql/backend/src/index.ts
@@ -0,0 +1,68 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    getAllBooks: [Book]
+    getOneBook: Book
+  }
+  type Mutation {
+    deleteBookById: String
+  }
+`;
+
+const booksDB = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const resolvers = {
+  Query: {
+    getAllBooks: () => booksDB,
+    getOneBook: () => booksDB[0],
+  },
+  Mutation: {
+    deleteBookById: () => "book was deleted",
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const start = async () => {
+  // Passing an ApolloServer instance to the `startStandaloneServer` function:
+  //  1. creates an Express app
+  //  2. installs your ApolloServer instance as middleware
+  //  3. prepares your app to handle incoming requests
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+};
+
+start();
